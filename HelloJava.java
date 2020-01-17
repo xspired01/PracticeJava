4ffdc5af66e9e4a6bc38d885f3a1a5f098876272
don spire <don.spire1@gmail.com>
2020-01-17 12:51:44 -0800

    update with swing

--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,10 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class HelloJava {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Hello, Java!");
+		JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
+		frame.add(label);
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 	}
