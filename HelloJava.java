7cb7a619eb2cf7e17ba70f4483f5b0ce4d03b705
don spire <don.spire1@gmail.com>
2020-01-17 14:18:32 -0800

    minor edits

--- HelloJava.java
+++ HelloJava.java
@@ -1,12 +1,19 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class HelloJava {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Hello, Java!");
-		JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
-		frame.add(label);
+
+		frame.add(new HelloComponent());
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 	}
 
+}
+
+class HelloComponent extends JComponent {
+	public void paintComponent(Graphics g) {
+		g.drawString("Hello, Java!", 125, 95);
+	}
 }
