66c6f00657aa56cf0b871e185157af873a797ae7
don spire <don.spire1@gmail.com>
2020-01-17 12:48:40 -0800

    add JFrame

--- HelloJava.java
+++ HelloJava.java
@@ -1,7 +1,10 @@
+import javax.swing.JFrame;
 
 public class HelloJava {
 	public static void main(String[] args) {
-		System.out.println("Hello, Java!");
+		JFrame frame = new JFrame("Hello, Java!");
+		frame.setSize(300, 300);
+		frame.setVisible(true);
 	}
 
 }
