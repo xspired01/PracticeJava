4470c2ad34747912c61741a2cf5a16c440c0278d
don spire <don.spire1@gmail.com>
2020-01-17 13:10:38 -0800

    add file expands hello component

---
+++ HelloJava2.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class HelloJava2 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloJava2");
+        frame.add(new HelloComponent2("Hello, Java2!"));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
+
+class HelloComponent2 extends JComponent implements MouseMotionListener {
+    String theMessage;
+    int messageX = 125, messageY = 95; // x and y axis for message
+
+    HelloComponent2(String message) {
+        theMessage = message;
+        addMouseMotionListener(this);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawString(theMessage, messageX, messageY);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        messageX = e.getX();
+        messageY = e.getY();
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    }
+}
